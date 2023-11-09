b2ee5e93d745ae49a39dd8a0c311ad5c45f0528b
Jordan <higgsfj@gmail.com>
2023-11-09 11:47:54 -0500

    made random letter generator and letter array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Global Variables
-let promptOptions = [];
+let letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 let score = 0;
 let letterSpeed = 4;
 let playArea = document.querySelector("#play-area")
@@ -9,7 +11,7 @@ const spawnArea = document.querySelector("#spawn-area");
 
 function letterTest () {
     let test = document.createElement("span")
-    test.innerText = "B";
+    test.innerText = randomLetter();
     test.classList.add("moving-down");
     test.style.animationDuration = convertingLetterSpeed(letterSpeed);
     playArea.appendChild(test);
@@ -17,6 +19,12 @@ function letterTest () {
 letterTest();
 
 setTimeout(letterTest, 1000)
+
+function randomLetter () {
+    let index;
+    index = Math.floor(Math.random() * letterOptions.length);
+    return letterOptions[index];
+}
 
 function convertingLetterSpeed (number) {
     return number + "s";
