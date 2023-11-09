7c2f8b6c035f91fbd06ffa1acd71ac643fa7612c
Jordan <higgsfj@gmail.com>
2023-11-09 14:35:49 -0500

    Updated the letter spawner with settimeout and deleting method

--- index.js
+++ index.js
@@ -1,31 +1,92 @@
 //Global Variables
 let letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
+let displayedLetters = []
+let spot = 0;
+let spotter;
 let score = 0;
-let letterSpeed = 4;
+let letterSpeed = 3;
 let playArea = document.querySelector("#play-area")
 let scoreDisplay = document.querySelector("#score-display");
 const spawnArea = document.querySelector("#spawn-area");
 
+class Letter {
+    constructor(letter) {
+        this.letter = randomLetter();
+    }
+    move() {
+        spot += .2;
+        spotter = spot + "%";
+        this.style.top = spotter;
+        if (spot < 100) {
+            requestAnimationFrame(move);
+        }
+        else {
+            thing.remove();
+        }
+    }
+}
 
-function letterTest () {
+//Spawning the letter
+function letterCreator() {
     let test = document.createElement("span")
     test.innerText = randomLetter();
     test.classList.add("moving-down");
+    setTimeout(() => {
+        test.remove();
+        displayedLetters.shift();
+        console.log(displayedLetters);
+    }, (letterSpeed - .2) * 1000);
     test.style.animationDuration = convertingLetterSpeed(letterSpeed);
-    playArea.appendChild(test);
+    spawnArea.appendChild(test);
+    displayedLetters.push(test);
+    console.log(displayedLetters);
 }
-letterTest();
+letterCreator();
+const interval = setInterval(letterCreator, 1000);
+clearInterval(interval);
 
-setTimeout(letterTest, 1000)
 
-function randomLetter () {
+
+
+
+
+
+
+function randomLetter() {
     let index;
     index = Math.floor(Math.random() * letterOptions.length);
     return letterOptions[index];
 }
 
-function convertingLetterSpeed (number) {
+function convertingLetterSpeed(number) {
     return number + "s";
 }
+
+
+
+
+
+//request Animation Frame method of animating the letters
+
+// function letterMove(timeStamp){
+//     let thing = spawnArea.children[0];
+//     spot += .2;
+//     spotter = spot + "%";
+//     thing.style.top = spotter;
+//     if (spot < 100) {
+//         requestAnimationFrame(letterMove);
+//     }
+//     else {
+//         thing.remove();
+//     }
+
+// }
+// requestAnimationFrame(letterMove);
+
+
+
+//Need to have every letter move down in intervals that get gradually faster
+//The letters are going to be in an array
+//Set interval for position
+//Looping for positon of the element
+//Display none for the position
