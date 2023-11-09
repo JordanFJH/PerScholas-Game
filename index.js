0b31556e07df8867a9d82f9592ce6b8cb1ea9d18
Jordan <higgsfj@gmail.com>
2023-11-09 11:12:21 -0500

    first version of letter moving down working

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 //Global Variables
 let score = 0;
 let playArea = document.querySelector("#play-area")
-let scoreDisplay = document.querySelector("score-display");
+let scoreDisplay = document.querySelector("#score-display");
+const spawnArea = document.querySelector("#spawn-area");
+
+
+function letterTest () {
+    let test = document.createElement("span")
+    test.innerText = "B";
+    test.classList.add("moving-down");
+    playArea.appendChild(test);
+}
+letterTest();
