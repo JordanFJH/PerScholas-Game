8a3da786877d837e4a8754f229f2423e379c37bd
Jordan <higgsfj@gmail.com>
2023-11-09 11:38:13 -0500

    Updated letter speed and letter positioning

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Global Variables
+let promptOptions = [];
 let score = 0;
+let letterSpeed = 4;
 let playArea = document.querySelector("#play-area")
 let scoreDisplay = document.querySelector("#score-display");
 const spawnArea = document.querySelector("#spawn-area");
@@ -9,6 +11,13 @@ function letterTest () {
     let test = document.createElement("span")
     test.innerText = "B";
     test.classList.add("moving-down");
+    test.style.animationDuration = convertingLetterSpeed(letterSpeed);
     playArea.appendChild(test);
 }
 letterTest();
+
+setTimeout(letterTest, 1000)
+
+function convertingLetterSpeed (number) {
+    return number + "s";
+}
